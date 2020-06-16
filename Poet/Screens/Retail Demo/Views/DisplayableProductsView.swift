aadbd60edfc3195c8c33f3aec17172028afc82cf
Steve Cotner <stevecotner@gmail.com>
2020-06-16 09:20:15 -0700

    Improving spacing for DisplayableProductsView

--- Poet/Screens/Retail Demo/Views/DisplayableProductsView.swift
+++ Poet/Screens/Retail Demo/Views/DisplayableProductsView.swift
@@ -13,21 +13,23 @@ struct DisplayableProductsView: View {
     let evaluator: FindingProductsEvaluating
     
     var body: some View {
-        return VStack(alignment: .leading, spacing: 40) {
-            ForEach(displayableProducts.array, id: \.id) { displayableProduct in
-                return HStack {
-                    VStack(alignment: .leading, spacing: 10) {
-                        ProductView(product: displayableProduct.product)
-                        if displayableProduct.findableProduct != nil {
-                            FoundNotFoundButtonsView(findableProduct: displayableProduct.findableProduct!, evaluator: self.evaluator)
-                                .padding(EdgeInsets(top: 8, leading: 20, bottom: 0, trailing: 20))
-                                .transition(.opacity)
+        return VStack(alignment: .leading, spacing: 0) {
+            VStack(alignment: .leading, spacing: 40) {
+                ForEach(displayableProducts.array, id: \.id) { displayableProduct in
+                    return HStack {
+                        VStack(alignment: .leading, spacing: 10) {
+                            ProductView(product: displayableProduct.product)
+                            if displayableProduct.findableProduct != nil {
+                                FoundNotFoundButtonsView(findableProduct: displayableProduct.findableProduct!, evaluator: self.evaluator)
+                                    .padding(EdgeInsets(top: 8, leading: 20, bottom: 0, trailing: 20))
+                                    .transition(.opacity)
+                            }
                         }
                     }
                 }
             }
             
-            Spacer().frame(height: .leastNonzeroMagnitude)
+            Spacer().frame(height: 30)
         }
     }
 }
